90f312d990b81f320e3a64bcc16ff13fd10d2726
roosta255 <roosta255@users.noreply.github.com>
2026-03-06 09:47:18 -0800

    fix: switched vite config to use loadEnv

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/ascii-game-frontend/',
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    base: command === 'serve' ? '/' : '/ascii-game-frontend/',
 
-  define: {
-    API_BASE: JSON.stringify(
-      command === 'serve'
-        ? '' // local dev uses proxy
-        : process.env.VITE_API_URL
-    )
-  },
+    define: {
+      API_BASE: JSON.stringify(
+        command === 'serve'
+          ? '' // local dev uses proxy
+          : env.VITE_API_URL
+      )
+    },
 
-  plugins: [react()],
+    plugins: [react()],
 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://game-backend.callawayservice.com',
-        changeOrigin: true,
-        secure: true,
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://game-backend.callawayservice.com',
+          changeOrigin: true,
+          secure: true,
+        }
       }
     }
   }
-}))
+})
